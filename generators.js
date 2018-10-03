972919ea568eaf2ad860d7925906f4b13b33b8c2
Nithyeswari <nithyeswari@gmail.com>
2018-10-03 11:24:48 +0100

    Create generators.js

---
+++ generators.js
@@ -0,0 +1,20 @@
+function* createQuoteFetcher() {
+  const response = yield fetch('http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json')
+  const quote = yield response.json()
+  return `${quote.quoteText} —${quote.quoteAuthor}`
+}
+
+const coroutine = (gen) => {
+  const generator = gen()
+
+  const handle = (result) => {
+    if (result.done) return Promise.resolve(result.value)
+    return Promise.resolve(result.value)
+      .then(res => handle(generator.next(res)))
+	}
+
+  return handle(generator.next())
+}
+
+const quoteFetcher = coroutine(createQuoteFetcher)
+quoteFetcher.then(quote => console.log(quote))
